d4001a0bb461be86bd09640662eb859508f5b0de
ciavash <svsh.kian@gmail.com>
2024-06-25 12:10:13 -0700

    styling hero & stats component

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -3,8 +3,8 @@ import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
 const Hero = () => (
-  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:p-16 px-6`}>
+  <section id="home" className={`flex md:flex-row flex-col `}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:p-16 px-1`}>
       <div className="flex flex-row items-center py-[6px] bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
         <p className={`${styles.paragraph}  px-2 `}>
@@ -14,8 +14,8 @@ const Hero = () => (
       </div>
       <div className={`flex flex-row justify-between items-center w-full `}>
         <h1
-          className={`flex-1 font-poppins font-semibold ss:text-[62px]
-           ss:leading-[75px] text-[52px] text-white`}>
+          className={`flex-1 font-poppins font-semibold ss:text-[42px]
+           ss:leading-[45px] text-[32px] text-white`}>
           The Next <br className="sm:block hidden" />{" "}
           <span className="text-gradient"> Generation </span>
         </h1>
@@ -24,8 +24,8 @@ const Hero = () => (
         </div>
       </div>
       <h1
-        className={` w-full font-poppins font-semibold ss:text-[62px]
-         ss:leading-[75px] text-[52px] text-white`}>
+        className={` w-full font-poppins font-semibold ss:text-[42px]
+         ss:leading-[25px] text-[52px] text-white`}>
         Payment Method
       </h1>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
@@ -38,13 +38,13 @@ const Hero = () => (
       <img
         src={robot}
         alt="robot"
-        className={`w-[100%] h-[100%]  z-[5] relative`}
+        className={`w-[80%] h-[80%]  z-[5] relative`}
       />
       <div
         className={`absolute  w-[40%] h-[35%]  top-0 pink__gradient z-[0]`}
       />
       <div
-        className={`absolute  w-[80%] h-[80%] rounded-full bottom-40  top-0 white__gradient z-[0]`}
+        className={`absolute  w-[40%] h-[40%] rounded-full bottom-40  top-0 white__gradient z-[0]`}
       />
       <div
         className={`absolute  w-[50%] h-[50%]  right-20 bottom-20 blue__gradient z-[0]`}
