b9f5fb892439793fdd87916c4027c3041078f815
ciavash <svsh.kian@gmail.com>
2024-06-25 12:54:02 -0700

    creating featureCard section

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -2,6 +2,26 @@ import React from "react";
 import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.index - 1 ? "mb-6" : "mb-0"
+    }feature-card`}>
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className={`w-[50%] h-[50%] object-contain`} />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins text-white font-semibold font-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins text-dimWhite font-normal font-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+);
+
 const Business = () => (
   <section id="features" className={`${layout.section}`}>
     <div className={`${layout.sectionInfo}`}>
@@ -10,15 +30,19 @@ const Business = () => (
         we'll handle the money...
       </h2>
       <p className={`${styles.paragraph} max-w-[470px]`}>
-      With
-        the right credit card, you can improve your financial life by building
-        credit, earning rewards and saving money. But with hundreds of credit
-        cards on the market. 100% Secured We take proactive steps make sure your
-        information and transactions are secure. Balance Transfer Get Started A
-        balance transfer credit card can save you a lot of money in interest
-        charges.
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds of
+        credit cards on the market. 100% Secured We take proactive steps make
+        sure your information and transactions are secure. Balance Transfer Get
+        Started A balance transfer credit card can save you a lot of money in
+        interest charges.
       </p>
       <Button styles="mt-10" />
+    </div>
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
     </div>
   </section>
 );
