b0e724958b0bfd6da1fde9c8033808c9d52cb970
ciavash <svsh.kian@gmail.com>
2024-06-26 09:53:53 -0700

    building billing component & styiling navbar

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -15,8 +15,8 @@ import {
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.paddingX}`}>
-        <div className={`${styles.boxWidth}`}>
+      <div className={`${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth} `}>
           <Navbar />
         </div>
       </div>
