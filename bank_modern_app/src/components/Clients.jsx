06842b9c911632052789e086ed697297278f8c50
ciavash <svsh.kian@gmail.com>
2024-06-26 10:50:10 -0700

    building  client component

--- bank_modern_app/src/components/Clients.jsx
+++ bank_modern_app/src/components/Clients.jsx
@@ -1,10 +1,22 @@
+import React from "react";
+import styles from "../style";
+import { clients } from "../constants";
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter}  w-full flex-wrap`}>
+      {clients.map((client, index) => (
+        <div
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] `}
+          key={client.id}>
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px]  w-[100px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-import React from 'react'
-
-function Clients() {
-  return (
-    <div>Clients</div>
-  )
-}
-
-export default Clients
+export default Clients;
